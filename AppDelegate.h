47120e5518edec2836184ee4e96a37991cb54fec
511308538@qq.com <511308538@qq.com>
2017-03-17 13:21:44 +0800

    source

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  timelineWithScrollview
+//
+//  Created by hennychen on 3/17/17.
+//  Copyright © 2017 hennychen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
